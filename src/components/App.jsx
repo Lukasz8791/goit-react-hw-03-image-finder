f13fd23689344665270c03f9f215352b1e51b0bd
Lukasz8791 <doktor8on@gmail.com>
2024-02-01 22:09:24 +0100

    polowa drogi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopBar from './TopBar/TopBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import styles from './App.module.css';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
 
 const App = () => {
   const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchImages = async (query, page) => {
+    return fetch(
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=41083655-82ce4b08f1604d0cb0165a8b6&image_type=photo&orientation=horizontal&per_page=12`
+    );
+  };
+
+  const loadImages = async (query, page) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetchImages(query, page);
+      if (response.ok) {
+        const data = await response.json();
+        if (currentPage === 1) {
+          setImages(data.hits);
+        } else {
+          setImages(prevImages => [...prevImages, ...data.hits]);
+        }
+      } else {
+        console.error('Error fetching images from Pixabay API');
+      }
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadImages(query, currentPage);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query, currentPage]);
 
   const handleFormSubmit = newQuery => {
+    setImages([]);
     setQuery(newQuery);
+    setCurrentPage(1);
+  };
+
+  const handleLoadMore = () => {
+    setCurrentPage(prevPage => prevPage + 1);
   };
 
   return (
     <div className={styles.app}>
       <TopBar onSubmit={handleFormSubmit} />
-      <ImageGallery query={query} />
+      <ImageGallery images={images} />
+
+      {isLoading && (
+        <div className={styles.loaderContainer}>
+          <Loader />
+        </div>
+      )}
+
+      {images.length > 0 && !isLoading && (
+        <Button onLoadMore={handleLoadMore} />
+      )}
     </div>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,105 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import styles from './ImageGallery.module.css';
-import Modal from '../Modal/Modal';
-import Loader from '../Loader/Loader';
 
-const ImageGallery = ({ query }) => {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [hasMore, setHasMore] = useState(true);
-
-  const fetchImages = async page => {
-    return fetch(
-      `https://pixabay.com/api/?q=${query}&page=${page}&key=41083655-82ce4b08f1604d0cb0165a8b6&image_type=photo&orientation=horizontal&per_page=12`
-    );
-  };
-
-  const loadImages = async () => {
-    setIsLoading(true);
-
-    try {
-      const response = await fetchImages(currentPage);
-      if (response.ok) {
-        const data = await response.json();
-        setImages(prevImages => [...prevImages, ...data.hits]);
-
-        if (data.hits.length < 12) {
-          setHasMore(false);
-        } else {
-          setCurrentPage(prevPage => prevPage + 1);
-        }
-      } else {
-        console.error('Error fetching images from Pixabay API');
-      }
-    } catch (error) {
-      console.error('Error fetching images:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    setImages([]);
-    setCurrentPage(1);
-    setHasMore(true);
-
-    if (query) {
-      loadImages();
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
-
-  const handleLoadMore = () => {
-    loadImages();
-  };
-
-  const handleImageClick = imageUrl => {
-    setSelectedImage(imageUrl);
-  };
-
-  const handleCloseModal = () => {
-    setSelectedImage(null);
-  };
-
-  return (
-    <div>
-      {isLoading && (
-        <div className={styles.loaderContainer}>
-          <Loader />
-        </div>
-      )}
-      <ul className={styles.gallery}>
-        {images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            image={image}
-            onClick={handleImageClick}
-          />
-        ))}
-      </ul>
-      {hasMore && !isLoading && (
-        <button
-          type="button"
-          onClick={handleLoadMore}
-          className={styles.loadMoreButton}
-        >
-          Load more
-        </button>
-      )}
-      {selectedImage && (
-        <Modal imageUrl={selectedImage} onClose={handleCloseModal} />
-      )}
-    </div>
-  );
-};
+const ImageGallery = ({ images, onClick }) => (
+  <div>
+    <ul className={styles.gallery}>
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} image={image} onClick={onClick} />
+      ))}
+    </ul>
+  </div>
+);
 
 ImageGallery.propTypes = {
-  query: PropTypes.string.isRequired,
+  images: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ImageGallery;
